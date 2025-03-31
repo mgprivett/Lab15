87c8ba1bddfce2674e38c1004de7f2f5105b360e
mgprivett <privett.matt@gmail.com>
2025-03-31 15:34:40 -0400

    First release for CSC 227

---
+++ tinysurvey.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/index.html', (request, response) => {
+  response.send('Hello from Index');
+})
+
+app.get('/extra.html', (request, response) => {
+    response.send('Hello from Extra');
+  })
+  
+app.get('/csc227.html', (request, response) => {
+    response.send('Hello from CSC 227')
+  })
+app.listen(3000, () => {
+  console.log("Express Server Running");
+})
